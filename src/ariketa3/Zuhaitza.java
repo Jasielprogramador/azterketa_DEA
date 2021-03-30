abcf8b41f0b0332e364c49ef310fa3d6e9a4f8e0
arrosa <asierrosa@gmail.com>
2021-03-30 22:37:36 +0200

    a

--- src/ariketa3/Zuhaitza.java
+++ src/ariketa3/Zuhaitza.java
@@ -22,17 +22,11 @@ public class Zuhaitza {
 	private ArrayList<Integer> lortuZerrenda(ArrayList<Integer> emaitza, BinaryTreeNode<Integer> unekoa) {
 		if(unekoa!=null) {
 			for(int i=0;i<unekoa.getBalioak().length;i++) {
-					emaitza = lortuZerrenda(emaitza, unekoa.getUmeak()[i]);
-					emaitza.add(unekoa.getBalioak()[i]);
+				emaitza = lortuZerrenda(emaitza, unekoa.getUmeak()[i]);
+				emaitza.add(unekoa.getBalioak()[i]);
 			}
 			lortuZerrenda(emaitza, unekoa.getUmeak()[unekoa.getBalioak().length]);
 		}
 		return emaitza;
-	}
-
-	private void gehitu(ArrayList<Integer> emaitza, Integer[] a){
-		for(int i = 0;i<a.length;i++){
-			emaitza.add(a[i]);
-		}
 	}
 }
