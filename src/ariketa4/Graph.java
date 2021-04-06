4d9c78570a11559b03ef1bfff506e683a9913666
arrosa <asierrosa@gmail.com>
2021-04-06 20:35:13 +0200

    amaituta

--- src/ariketa4/Graph.java
+++ src/ariketa4/Graph.java
@@ -1,5 +1,6 @@
 package ariketa4;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -15,22 +16,73 @@ public class Graph {
         Queue<Integer> aztertuGabeak = new LinkedList<Integer>();
         boolean[] aztertuak = new boolean[numVertices];
         boolean maila = true;
+        ArrayList<String> emaitza = new ArrayList<String>();
+
+        aztertuGabeak.add(0);
         
         while(!aztertuGabeak.isEmpty()) {
         	Integer unekoa = aztertuGabeak.remove();
+
         	if(lag.size()==0) {
         		lag.add(true);
         	}
         	else {
         		lag.add(!maila);
         	}
-        	
-        	for(int i = 0;i<vertices.length;i++) {
-        		if(adjMatrix[unekoa][i]==1 && aztertuak[i] == false) {
-        			
-        		}
-        	}
+
+            if (vertices[unekoa] == pelikula) {
+
+                for (int i = 0; i < vertices.length; i++) {
+                    if (adjMatrix[unekoa][i] == 1 && aztertuak[i] == false) {
+                        aztertuGabeak.add(adjMatrix[unekoa][i]);
+                        aztertuak[adjMatrix[unekoa][i]] = true;
+                    }
+                }
+            }
         }
-        
+
+        for (int i = 0;i<lag.size();i++){
+            if(lag.get(i) == true){
+                emaitza.add(vertices[i]);
+            }
+        }
+        return emaitza;
+    }
+
+    public ArrayList<String> lortuPelikulak2(String pelikula){
+        ArrayList<Boolean> lag = new ArrayList<Boolean>();
+        Queue<Integer> aztertuGabeak = new LinkedList<Integer>();
+        boolean[] aztertuak = new boolean[numVertices];
+        boolean maila = true;
+        ArrayList<String> emaitza = new ArrayList<String>();
+        aztertuGabeak.add(0);
+
+        ArrayList<Integer> aktoreak = new ArrayList<Integer>();
+        ArrayList<String> pelikulak = new ArrayList<String>();
+
+        while(!aztertuGabeak.isEmpty()) {
+            Integer unekoa = aztertuGabeak.remove();
+
+            if (vertices[unekoa] == pelikula) {
+
+                for (int i = 0; i < vertices.length; i++) {
+                    if (adjMatrix[unekoa][i] == 1 && aztertuak[i] == false) {
+                        aktoreak.add(i);
+                        aztertuak[i] = true;
+                    }
+                }
+
+                for (Integer akt : aktoreak) {
+                    for (int i = 0; i < vertices.length; i++) {
+                        if (adjMatrix[akt][i] == 1 && !aztertuak[i]) {
+                            pelikulak.add(vertices[i]);
+                            aztertuak[i] = true;
+                        }
+                    }
+                }
+            }
+        }
+        return pelikulak;
+
     }
 }
